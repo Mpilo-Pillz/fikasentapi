d8573378ca83ef4f5bfebc1990b38e62ba6d6f54
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-09-29 20:20:56 +0200

    adding project to cv module

--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrainingModule } from './training/training.module';
+import { ProjectService } from './project/project.service';
+import { ProjectController } from './project/project.controller';
 
 @Module({
-  imports: [TrainingModule]
+  imports: [TrainingModule],
+  providers: [ProjectService],
+  controllers: [ProjectController]
 })
 export class CvModule {}
