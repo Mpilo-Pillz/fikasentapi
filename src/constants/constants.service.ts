90d7b4dbd804f8711fd2e38b1e51274e47fe122c
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-08 13:32:45 +0200

    adding cv module

--- src/constants/constants.service.ts
+++ src/constants/constants.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ConstantsService {
-  CRYPTO_BASE_URL = process.env.CRYPTO_BASE_URL;
+  CRYPTO_BASE_URL = 'https://rest.coinapi.io/v1/exchangerate';
   CRYPTO_API_KEY = process.env.CRYPTO_API_KEY;
-  WEATHER_BASE_URL = process.env.WEATHER_BASE_URL;
+  WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+  DB_URL = process.env.DB_URL;
 }
--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TrainingModule } from './training/training.module';
 import { ProjectService } from './project/project.service';
 import { ProjectController } from './project/project.controller';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [TrainingModule],
-  providers: [ProjectService],
-  controllers: [ProjectController]
+  imports: [TrainingModule, ProjectModule],
 })
 export class CvModule {}
