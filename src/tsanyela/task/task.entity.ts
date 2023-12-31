fcb137dce02674f871e609388bde604c604bd436
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-31 02:23:08 +0200

    adding task entity and controller

---
+++ src/tsanyela/task/task.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Staff } from '../staff/staff.entity';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  date: Date;
+
+  @Column()
+  status: string;
+
+  @ManyToOne(() => Staff, (staff) => staff.tasks)
+  staff: Staff;
+
+  @Column()
+  staffId: number;
+}
