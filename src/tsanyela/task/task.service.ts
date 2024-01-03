c851c1950265f2708a44aac2090c58476f1fab4b
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-04 00:07:38 +0200

    creating task service

--- src/tsanyela/task/task.service.ts
+++ src/tsanyela/task/task.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto, UpdateTaskDto } from './task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createTaskDto: CreateTaskDto) {
+    return await this.prisma.task.create({ data: createTaskDto });
+  }
+
+  async findAll() {
+    return await this.prisma.task.findMany();
+  }
+
+  async findOne(id: number) {
+    const task = await this.prisma.task.findUnique({ where: { id } });
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+    return task;
+  }
+
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    try {
+      return await this.prisma.task.update({
+        where: { id },
+        data: updateTaskDto,
+      });
+    } catch (error) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      await this.prisma.task.delete({ where: { id } });
+    } catch (error) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+  }
+}
