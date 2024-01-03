bc5b778922b44024e081b2a055e62de31846f258
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-03 20:32:39 +0200

    creating staff controller

--- src/tsanyela/staff/staff.controller.ts
+++ src/tsanyela/staff/staff.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
+import { StaffService } from './staff.service';
+import { CreateStaffDto, UpdateStaffDto } from './staff.dto';
 
 @Controller('staff')
-export class StaffController {}
+export class StaffController {
+  constructor(private readonly staffService: StaffService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createStaffDto: CreateStaffDto) {
+    return await this.staffService.create(createStaffDto);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.staffService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.staffService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateStaffDto: UpdateStaffDto,
+  ) {
+    return await this.staffService.update(id, updateStaffDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    await this.staffService.remove(id);
+    return;
+  }
+}
