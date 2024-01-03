3963b70f1f30c0abfe758cf58453cc766b9b8fdd
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-04 00:07:51 +0200

    creating task controller

--- src/tsanyela/task/task.controller.ts
+++ src/tsanyela/task/task.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto, UpdateTaskDto } from './task.dto';
 
-@Controller('task')
-export class TaskController {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return await this.taskService.create(createTaskDto);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.taskService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.taskService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    return await this.taskService.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    await this.taskService.remove(id);
+    return;
+  }
+}
